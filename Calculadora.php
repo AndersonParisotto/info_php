b861f2027f53a6f7c8202b1c9a07e0a6dfe0adaa
AndersonParisotto <ALParisotto@ucs.br>
2023-07-25 21:57:29 -0300

    Add expo. e verif. par ou impar

--- Calculadora.php
+++ Calculadora.php
@@ -1,7 +1,7 @@
 <?php
-$value = '';
-$operator = '';
-$value2 = '';
+$value = '2';
+$operator = '^';
+$value2 = '3';
 $result = 0;
 
 if ($operator == '/') {
@@ -16,13 +16,20 @@ else if ($operator == '+') {
     $result = $value + $value2 ;
 }
 
+else if ($operator == '^'){
+    $result = $value ** $value2 ;
+}
+
 else {
     $result = $value - $value2 ;
 }
 
-echo ("Resultado de {$value} {$operator} {$value2} = {$result}");
+echo ("Resultado de {$value} {$operator} {$value2} = {$result} <br> " );
 
-
-
-
+if ($result % 2 == 0){
+    echo ("O número {$result} é Par ") ;
+}
+else {
+    echo ("O número {$result} é Impar ") ;
+}
 ?>
