6cd324f86341ba7def454ef64db22d16ac778643
AndersonParisotto <ALParisotto@ucs.br>
2023-08-01 21:48:48 -0300

    Adição de Desconto, acrescimo, media aritmetica e harmonica

--- Calculadora.php
+++ Calculadora.php
@@ -1,35 +1,93 @@
 <?php
-$value = '2';
-$operator = '^';
-$value2 = '3';
+
+/*
+------Operadores------
++ = Adição
+- = Diminuição
+* = Multiplicação
+/ = Divisão
+^ = Potenciação
+%- = Desconto
+%+ = Acrescimo
+arit = Media Aritmetica
+harm = Média Harmônica
+*/
+
+$value = 4;
+$operator = 'harm';
+$value2 = 5;
 $result = 0;
 
-if ($operator == '/') {
+if ($operator == '/') 
+{
     $result = $value / $value2 ;
 }
 
-else if ($operator == '*') {
+else if ($operator == '*') 
+{
     $result = $value * $value2 ;
 }
 
-else if ($operator == '+') {
+else if ($operator == '+')
+{
     $result = $value + $value2 ;
 }
 
-else if ($operator == '^'){
+else if ($operator == 'arit')
+{
+    $result = ($value + $value2) / 2 ;
+    echo ("Média aritmética entre {$value} e {$value2} é de {$result}") ;
+    exit ;
+}
+
+else if ($operator == 'harm')
+{
+    $result = round(2 / ((1/$value) + (1/$value2)), 2);
+    echo ("Média harmônica entre {$value} e {$value2} é de {$result}") ;
+    exit ;
+}
+ 
+else if ($operator == '^')
+{
     $result = $value ** $value2 ;
 }
 
-else {
+else if ($operator == '%-')
+{
+    $result = $value - ($value * ($value2 / 100)) ;
+    echo ("{$value} com {$value2}% de desconto é de {$result}") ;
+    exit ;
+}
+
+else if ($operator == '%+')
+{
+    $result = $value + ($value * ($value2 / 100)) ;
+    echo ("{$value} com {$value2}% de acrescimo é de {$result}") ;
+    exit ;
+}
+
+else if ($operator == 'mmc')
+{
+   echo ('Não tem') ;
+   exit ;
+}
+
+else 
+{
     $result = $value - $value2 ;
 }
 
 echo ("Resultado de {$value} {$operator} {$value2} = {$result} <br> " );
 
-if ($result % 2 == 0){
+if ($result % 2 == 0)
+{
     echo ("O número {$result} é Par ") ;
 }
-else {
+
+else 
+{
     echo ("O número {$result} é Impar ") ;
 }
-?>
+
+
+    
