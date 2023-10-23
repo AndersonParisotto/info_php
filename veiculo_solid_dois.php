8c97e520263c26382514aeae530a539a387a66e0
AndersonParisotto <131724081+AndersonParisotto@users.noreply.github.com>
2023-10-23 19:28:18 -0300

    Update veiculo_solid_dois.php

--- veiculo_solid_dois.php
+++ veiculo_solid_dois.php
@@ -81,3 +81,89 @@ $moto->dirigir($cahve);
 
 $veiculo = new Veiculo();
 echo $veiculo->locomover($moto);
+
+
+/*<?php
+
+interface IVeiculo {
+    public function dirigir($chave);
+}
+
+class Veiculo {
+    public $veiculo;
+
+    public function locomover(IVeiculo $veiculo) {
+        $this->veiculo = $veiculo;
+    }
+
+    public function __toString() {
+        return (string)$this->veiculo;
+    }
+}
+
+class Carro implements IVeiculo {
+    public $chave = "chave_presencial";
+    public $airbag = "motorista_carona";
+
+    public function dirigir($chave) {
+        $this->chave = $chave;
+        echo "estou andando... {$this->chave}<br>";
+    }
+    
+    public function setAirBag($airbag) {
+        $this->airbag = $airbag;
+        echo "o airbag é... {$this->airbag}<br>";
+    }
+    
+}
+
+class Trator implements IVeiculo {
+    public $chave = "chave_normal";
+
+    public function dirigir($chave) {
+        $this->chave = $chave;
+        echo "estou andando... {$this->chave}<br>";
+    }
+}
+
+class Carreta implements IVeiculo {
+    public $chave = "chave_canivete";
+
+    public function dirigir($chave) {
+        $this->chave = $chave;
+        $oleo = $this->verificarOleoMotor();
+        echo "estou andando... {$this->chave}<br>" . $oleo;
+    }
+
+    public function verificarOleoMotor() {
+        return "Oleo esta OK!<br>";
+    }
+
+}
+
+
+class Moto implements IVeiculo {
+    public $chave = "chave_normal";
+
+    public function dirigir($chave) {
+        $this->chave = $chave;
+        echo "estou andando... {$this->chave}<br>";
+    }
+}
+
+$chave = "xyz";
+
+$carro = new Carro();
+$carro->dirigir($chave);
+$carro->setAirBag("airbag_lateral");
+
+$carreta = new Carreta();
+$carreta->dirigir("abc");
+
+
+$moto = new Moto();
+$moto->dirigir($chave);
+
+$veiculo = new Veiculo();
+$veiculo->locomover($moto);
+echo $veiculo;*/
